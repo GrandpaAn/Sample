76d63202108d306486e577533215ad59390d575b
GrandpaAn <2236327498@qq.com>
2018-03-15 22:06:38 +0800

    day2

--- Sample.py
+++ Sample.py
@@ -1,7 +1,15 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.routing import BaseConverter
+
+class RegexConverter(object):
+	"""docstring for RegexConverter"""
+	def __init__(self, url_map, *items):
+		super(RegexConverter,self).__init__()
+		self.regex=items[0]
 
 app = Flask(__name__)
+app.url_map.converters['regex'] = RegexConverter
 
 @app.route('/')
 def index():
@@ -14,6 +22,19 @@ def serivce():
 @app.route('/about')
 def about():
 	return 'About'
+#路由
+# @app.route('/user/<regex("[a-z]{3}"):user_id>')
+# def user(user_id):
+# 	return 'User ID %s' % user_id
+
+@app.route('/projects/')
+@app.route('/project-page/')
+def projects():
+	return 'The project page'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	return render_template('login.html', method=request.method)
 
 if __name__ == '__main__':
 	app.run(debug=True)
