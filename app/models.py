f249cecee9e73293e95922dc88f05f0e6f3a43db
GrandpaAn <2236327498@qq.com>
2018-03-17 16:05:15 +0800

    day14_1

--- app/models.py
+++ app/models.py
@@ -1,14 +1,15 @@
+# coding: utf-8
 from . import db
 
 class Role(db.Model):
 	__tablename__ = 'roles'
-	id = db.Column(db.Integer, primany_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String, nullable=True)
-	users = db.relationship('User', bachref='roles')
+	users = db.relationship('User', backref='role')
 
 class User(db.Model):
 	__tablename__ = 'users'
-	id = db.Column(db.Integer, primany_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String, nullable=True)
 	password = db.Column(db.String, nullable=True)
-	role_id = db.Column(db.Integer, db.Foreignkey('roles.id'))
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
